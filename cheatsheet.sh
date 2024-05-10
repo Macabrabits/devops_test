b5d2f7c687820cedac010d5ffc923b1e2cb50e96
Victor Mattar <victor.mattar@zenvia.com>
2024-05-10 19:04:20 -0300

    releases artifacts

--- cheatsheet.sh
+++ cheatsheet.sh
@@ -11,3 +11,13 @@ docker tag nttdata_test macabrabits/nttdata_test:1
 docker push macabrabits/nttdata_test:1
 docker run --rm -it nttdata_test
 
+git tag v0.0.1
+git tag -d v0.0.2 rm
+git push origin --tags
+git reset --hard 93521052a89ffaf88bff4796ce13e8c21df7cb94
+git reset --soft 93521052a89ffaf88bff4796ce13e8c21df7cb94
+git add . && git commit --amend --no-edit && git push -f
+git add . && git commit --amend --no-edit && git push -f --tags
+
+
+git tag v0.0.4
